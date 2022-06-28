43c643cb7703f228ea90b7511ac7c43c569bae88
OProgramadorJunior <107969523+OProgramadorJunior@users.noreply.github.com>
2022-06-27 23:41:29 -0300

    add testes;

---
+++ src/models/main.ts
@@ -0,0 +1,13 @@
+/*
+Um time possui um nome e jogadores que podem ser adicionados ou removidos. Um time completo é formado por
+5 jogadores, sendo 1 goleiro, 2 defensores e 2 atacantes. Além disso, o programa deve controlar os resultados
+obtidos pelo time (vitórias, empates e derrotas). Ao final de cada partida, os resultados de cada time devem ser
+atualizados. Uma vitória vale 3 pontos, empate vale 1 e derrota não pontua.
+Uma partida é composta de dois times, onde um é o time da casa e o outro é o visitante. Além da data da partida,
+o programa deve guardar o placar da partida (quantos gols o time da casa fez e quantos gols o time visitante fez).
+Desta forma, será possível saber quem venceu a partida ou se houve empate.
+O programa deve permitir cadastrar várias partidas e depois simular a execução destas partidas, atualizando
+estatísticas de times e jogadores. Na simulação, considere a pontuação total das habilidades de cada time (quem
+tem a maior pontuação, tem mais chance de sair vitorioso). Além disso, se o time que estiver jogando em casa tem
+uma chance adicional. Você pode definir sua própria fórmula, mas ela deve estar explicada no código (javadoc).
+*/
