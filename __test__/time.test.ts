3f19591f5bd1d23cfd0d5fd08468d61a9bceadd6
OProgramadorJunior <107969523+OProgramadorJunior@users.noreply.github.com>
2022-06-29 22:30:12 -0300

    ajustes;

--- __test__/time.test.ts
+++ __test__/time.test.ts
@@ -14,40 +14,27 @@ describe('testes de cadastros', () => {
     expect(Chapecoense.getNome()).toEqual('Chapecoense');
   });
 
-  let RealMadrid = new Time('RealMadrid');
-  let aReal1 = new JogadorAtacante(82, 90, "Romario", "2000-07-20", 10);
-  let aReal2 = new JogadorAtacante(70, 83, "Jorge", "1997-08-02", 9);
-  let dReal1 = new JogadorDefensor(55, 81, "Tobias", "1997-08-02", 29);
-  let dReal2 = new JogadorDefensor(50, 84, "Rogerio", "1997-08-02", 32);
-  let gReal = new JogadorGoleiro(184, 72, "Timuca", "1997-08-02", 7);
   
-  test('cadastrar vários times', () => {
-    RealMadrid.setarJogador(aReal1);
-    RealMadrid.setarJogador(aReal2);
-    RealMadrid.setarJogador(dReal1);
-    RealMadrid.setarJogador(dReal2);
-    RealMadrid.setarJogador(gReal);
-
-    let jogador1 = RealMadrid.getJogadores(); 
-
-
-    expect(jogador1[0]).toEqual(aReal1);
-    expect(jogador1[1]).toEqual(aReal2);
-    expect(jogador1[2]).toEqual(dReal1);
-    expect(jogador1[3]).toEqual(dReal2);
-    expect(jogador1[4]).toEqual(gReal);
-
+  test('adicionar jogadores em um time', () => {
+    let RealMadrid = new Time('RealMadrid');
+    RealMadrid.setarJogador(new JogadorAtacante(82, 90, "Romario", "2000-07-20", 10));
+    RealMadrid.setarJogador(new JogadorAtacante(70, 83, "Jorge", "1997-08-02", 9));
+    RealMadrid.setarJogador(new JogadorDefensor(55, 81, "Tobias", "1997-08-02", 29));
+    RealMadrid.setarJogador(new JogadorDefensor(50, 84, "Rogerio", "1997-08-02", 32));
+    RealMadrid.setarJogador(new JogadorGoleiro(184, 72, "Timuca", "1997-08-02", 7));
+    expect(RealMadrid.getJogadores().length).toEqual(5);
   });
 
-  test('cadastrar vários times', () => {
-    RealMadrid.removerJogador(aReal1);
-    RealMadrid.removerJogador(aReal2);
-    RealMadrid.removerJogador(dReal1);
-    RealMadrid.removerJogador(dReal2);
-    RealMadrid.removerJogador(gReal);
-
-    expect(RealMadrid.getTimeCompleto()).toEqual(false);
-
+  test('remover jogadores de um time', () => {
+    let RealMadrid = new Time('RealMadrid');
+    RealMadrid.setarJogador(new JogadorAtacante(82, 90, "Romario", "2000-07-20", 10));
+    RealMadrid.setarJogador(new JogadorAtacante(70, 83, "Jorge", "1997-08-02", 9));
+    RealMadrid.setarJogador(new JogadorDefensor(55, 81, "Tobias", "1997-08-02", 29));
+    RealMadrid.setarJogador(new JogadorDefensor(50, 84, "Rogerio", "1997-08-02", 32));
+    RealMadrid.setarJogador(new JogadorGoleiro(184, 72, "Timuca", "1997-08-02", 7));
+    RealMadrid.removerJogador(RealMadrid.getJogadores()[0]);
+    RealMadrid.removerJogador(RealMadrid.getJogadores()[0]);
+    expect(RealMadrid.getJogadores().length).toEqual(3);
   });
 
 
